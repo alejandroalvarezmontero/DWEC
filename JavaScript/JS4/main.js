1bc1f32210996c4368b2d7f016de51a17d786ee8
alejandroalvarezmontero <95092635+alejandroalvarezmontero@users.noreply.github.com>
2022-12-14 19:43:50 +0100

    Varicaciones
    Retoques en el estilo
    y añadir imagenes

--- JavaScript/JS4/main.js
+++ JavaScript/JS4/main.js
@@ -8,7 +8,7 @@ const pokeStats = document.querySelector('[data-poke-stats]');
 
 const typeColors = {electric: '#FFEA70',    normal: '#B09398',    fire: '#FF675C',water: '#0596C7',    ice: '#AFEAFD',    rock: '#999799',    flying: '#7AE7C7',    grass: '#4A9681',    psychic: '#FFC6D9',ghost: '#561D25',bug: '#A2FAA3',poison: '#795663',ground: '#D2B074',dragon: '#DA627D',steel: '#1D8A99',fighting: '#2F2F2F', default: '#2A1A1F',
 };
-const searchPokemon = event => {
+const buscaPokemon = event => {
     event.preventDefault();
     const { value } = event.target.pokemon;
     fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
@@ -30,8 +30,8 @@ const renderPokemonData = data => {
 const setCardColor = types => {
     const colorOne = typeColors[types[0].type.name];
     const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
-    pokeImg.style.background =  `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
-    pokeImg.style.backgroundSize = ' 5px 5px';
+    pokeImg.style.background =  `radial-gradient(${colorTwo} 53%, ${colorOne} 53%)`;
+    pokeImg.style.backgroundSize = ' 100px 100px';
 }
 
 const renderPokemonTypes = types => {
@@ -61,7 +61,7 @@ const renderPokemonStats = stats => {
 const renderNotFound = () => {
     pokeName.textContent = 'No encontrado';
     pokeImg.setAttribute('src', 'poke-shadow.png');
-    pokeImg.style.background =  '#fff';
+  
     pokeTypes.innerHTML = '';
     pokeStats.innerHTML = '';
     pokeId.textContent = '';
