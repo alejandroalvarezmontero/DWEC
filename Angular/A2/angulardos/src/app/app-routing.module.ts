8543aed3e4e03b7148663fb26880e60db97cafc5
alejandroalvarezmontero <95092635+alejandroalvarezmontero@users.noreply.github.com>
2023-02-03 17:37:01 +0100

    Imports
    Los imports que se crearon en app.component.ts automaticamente se lo metemos a el archivo app-routing.
    Y en el path ponemos el nombre que queremos poner para despues de la "/" y luego component: su nombre predeterminado  en app.component.ts

--- Angular/A2/angulardos/src/app/app-routing.module.ts
+++ Angular/A2/angulardos/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoggerComponent } from './logger/logger.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { OlivdarPassComponent } from './olivdar-pass/olivdar-pass.component';
+import { VerificarComponent } from './verificar/verificar.component';
+
+
+const routes: Routes = [
+  { path: 'home', component : HomeComponent },
+  { path: 'login', component: LoggerComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'recuperacion', component:  OlivdarPassComponent },
+  { path: 'verificar', component: VerificarComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
