521c40f76b0382c4965177d9c2da9fe0488893c1
alejandroalvarezmontero <95092635+alejandroalvarezmontero@users.noreply.github.com>
2023-02-17 17:04:18 +0100

    Pokeapi

--- Angular/A3A4/angulartrecuatro/src/app/app-routing.module.ts
+++ Angular/A3A4/angulartrecuatro/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoggerComponent } from './logger/logger.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { OlvidarPassComponent } from './olvidar-pass/olvidar-pass.component';
+import { VerificarComponent } from './verificar/verificar.component';
+
+
+const routes: Routes = [
+  { path: 'home', component : HomeComponent },
+  { path: 'login', component: LoggerComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'recuperacion', component:  OlvidarPassComponent },
+  { path: 'verificar', component: VerificarComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Angular/A3A4/angulartrecuatro/src/app/olvidar-pass/olvidar-pass.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-olvidar-pass',
+  templateUrl: './olvidar-pass.component.html',
+  styleUrls: ['./olvidar-pass.component.css']
+})
+export class OlvidarPassComponent {
+
+}
